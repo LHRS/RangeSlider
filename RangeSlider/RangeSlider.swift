5c422905ae19c7a833ff0ee6fa3028017e88334b
Leslie Hon <leslie.hon@redso.com.hk>
2016-10-12 12:19:29 +0800

    Customize

--- RangeSlider/RangeSlider.swift
+++ RangeSlider/RangeSlider.swift
@@ -61,12 +61,12 @@ class RangeSliderThumbLayer: CALayer {
             return
         }
         
-        let thumbFrame = bounds.insetBy(dx: 2.0, dy: 2.0)
+        let thumbFrame = bounds.insetBy(dx: 1.0, dy: 1.0)
         let cornerRadius = thumbFrame.height * slider.curvaceousness / 2.0
         let thumbPath = UIBezierPath(roundedRect: thumbFrame, cornerRadius: cornerRadius)
         
         // Fill
-        ctx.setFillColor(slider.thumbTintColor.cgColor)
+        ctx.setFillColor(slider.trackHighlightTintColor.cgColor)
         ctx.addPath(thumbPath.cgPath)
         ctx.fillPath()
         
